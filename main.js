ec7d5c20752d172b626ef69f4e8267d3bd1538b7
Azamatjon <azamatergasev01@gmail.com>
2024-04-24 11:14:36 +0500

    first task done

---
+++ main.js
@@ -0,0 +1,50 @@
+// Qoshimcha bajarilgan tasklar
+// let arr =  [
+//     {
+//     title: "Halqa",
+//     author: "Akrom Malik",
+//     read: false
+//     },
+//     {
+//         title: "Baxtiyor oila",
+//         author: "Shayx Muhammad Sodiq Muhammad Yusuf Raximatulloh",
+//         read : true
+//     },
+//     {
+//     title: "Dunyoning ishlari",
+//     author: "O’tkir Hoshimov",
+//     read: true
+//     },
+//     {
+//     title: "Iymon",
+//     author: "Shayx Muhammad Sodiq Muhammad Yusuf",
+//     read: true
+//     },
+//     ]
+
+//     arr.forEach(item => {
+//         if (item.read) {
+//             console.log(` o'qilgan kitoblar ${item.title}`);
+//         }else{
+//             console.log(`  oqilmagan kitoblar  ${item.title} `);
+//         }
+//     })
+
+// 51 task starts
+
+// function findMiddleValue () {
+//     let arr = [1,2,3,4,6,5,4,3,9]
+//     let count = arr.length
+
+//     if (count % 2 !== 0){ 
+//         const middleNumber = (count / 2)
+//         return middleNumber
+//     }else{
+//         const middleIndex = count / 2
+//         return middleIndex
+// }
+// }
+
+// console.log(findMiddleValue()) 
+
+// 51 task finished 
